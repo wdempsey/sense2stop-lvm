9a7890ae1be7dea0435624aa7a3dc04ca9192015
wdempsey <dempsey.walter@gmail.com>
2019-03-16 16:14:19 -0400

    fixed datetime tz issue

--- data_preprocessing/unzip_functions.py
+++ data_preprocessing/unzip_functions.py
@@ -5,22 +5,30 @@ import json
 import os
 import os.path
 import bz2
+import pytz
 
 def unix_date(intime):
-    return ( datetime.fromtimestamp(int(intime)/
-                                   1000).strftime('%Y-%m-%d %H:%M:%S'))
+    local_tz = pytz.timezone('US/Central')
+    date = datetime.fromtimestamp(int(intime)/1000, local_tz)
+    return date.strftime('%Y-%m-%d %H:%M:%S')
 
 
 def hour_of_day(intime):
-    return (datetime.fromtimestamp(int(intime)/1000).strftime("%H"))
+    local_tz = pytz.timezone('US/Central')
+    date = datetime.fromtimestamp(int(intime)/1000, local_tz)
+    return date.strftime("%H")
 
 
 def minute_of_day(intime):
-    return (datetime.fromtimestamp(int(intime)/1000).strftime("%M"))
+    local_tz = pytz.timezone('US/Central')
+    date = datetime.fromtimestamp(int(intime)/1000, local_tz)
+    return date.strftime("%M")
 
 
 def day_of_week(intime):
-    return (datetime.fromtimestamp(int(intime)/1000).strftime("%A"))
+    local_tz = pytz.timezone('US/Central')
+    date = datetime.fromtimestamp(int(intime)/1000, local_tz)
+    return date.strftime("%A")
 
 
 def days_between(d1, d2):
@@ -30,7 +38,9 @@ def days_between(d1, d2):
 
 
 def date_of_month(intime):
-    return (datetime.fromtimestamp(int(intime)/1000).strftime('%Y-%m-%d'))
+    local_tz = pytz.timezone('US/Central')
+    date = datetime.fromtimestamp(int(intime)/1000, local_tz)
+    return date.strftime('%Y-%m-%d')
 
 def to_likert(instring):
     if instring=="NO!!!":
@@ -232,11 +242,14 @@ def strip_end_of_day_ema_json(json_data):
     if (json_data['status'] == 'COMPLETED' or
         json_data['status'] == 'ABANDONED_BY_TIMEOUT'):
         response = json_data['question_answers'][0]['response']
-        for block in hourblocks:
-            if block in response:
-                binary_outcome.extend(['1'])
-            else:
-                binary_outcome.extend(['0'])
+        if response is not None:
+            for block in hourblocks:
+                if block in response:
+                    binary_outcome.extend(['1'])
+                else:
+                    binary_outcome.extend(['0'])
+        else:
+            binary_outcome.extend(['NA'] * len(hourblocks))
     else:
         binary_outcome.extend(['NA'] * len(hourblocks))
     return binary_outcome
@@ -376,3 +389,71 @@ def strip_event_contingent_json(json_data):
     else:
         data.extend(['NA'] * (len(htm_questions) + len(ratings_questions)) )
     return data
+
+
+def cstress(participant_zip, participant_id):
+    # Inputs: zipfile, participant_id
+    # Output: add to csv (prints when done)
+    zip_namelist = participant_zip.namelist()
+    bz2_marker = 'STRESS_LABEL+PHONE.csv.bz2'
+    zip_matching = [s for s in zip_namelist if bz2_marker in s]
+    bz2_file = participant_zip.open(zip_matching[0])
+    temp = bz2_file.read()
+    if not temp or temp == 'BZh9\x17rE8P\x90\x00\x00\x00\x00':
+        return None
+    else:
+        bz2_file = participant_zip.open(zip_matching[0])
+        newfile = pd.read_csv(bz2_file, compression='bz2', header=None)
+        df = pd.DataFrame(np.array(newfile).reshape(-1, 3),
+                          columns=['timestamp', 'offset', 'event'])
+        df['participant_id'] = participant_id
+        df['date'] = df['timestamp'].apply(unix_date)
+        df['hour'] = df['timestamp'].apply(hour_of_day)
+        df['minute'] = df['timestamp'].apply(minute_of_day)
+        df['day_of_week'] =  df['timestamp'].apply(day_of_week)
+        save_dir = '/Users/walterdempsey/Box/MD2K Processed Data/smoking-lvm-cleaned-data/'
+        save_filename = 'stress-label.csv'
+        if os.path.isfile(save_dir + save_filename):
+            append_write = 'a'  # append if already exists
+            header_binary = False
+        else:
+            append_write = 'w'  # make a new file if not
+            header_binary = True
+        temp_csv_file = open(save_dir+save_filename, append_write)
+        df.to_csv(temp_csv_file, header=header_binary, index=False)
+        temp_csv_file.close()
+        print('Added to stress label file!')
+        return None
+
+def stress_episodes(participant_zip, participant_id):
+    # Inputs: zipfile, participant_id
+    # Output: add to csv (prints when done)
+    zip_namelist = participant_zip.namelist()
+    bz2_marker = 'CSTRESS_STRESS_EPISODE_ARRAY_CLASSIFICATION_FULL_EPISODE+PHONE.csv.bz2'
+    zip_matching = [s for s in zip_namelist if bz2_marker in s]
+    bz2_file = participant_zip.open(zip_matching[0])
+    temp = bz2_file.read()
+    if not temp or temp == 'BZh9\x17rE8P\x90\x00\x00\x00\x00':
+        return None
+    else:
+        bz2_file = participant_zip.open(zip_matching[0])
+        newfile = pd.read_csv(bz2_file, compression='bz2', header=None)
+        df = pd.DataFrame(np.array(newfile).reshape(-1, 6),
+                          columns=['timestamp', 'offset', 'start_ts', 'peak_ts', 'end_ts', 'episode_label'])
+        df['participant_id'] = participant_id
+        df['start_date'] = df['start_ts'].apply(unix_date)
+        df['peak_date'] = df['peak_ts'].apply(unix_date)
+        df['end_date'] = df['end_ts'].apply(unix_date)
+        save_dir = '/Users/walterdempsey/Box/MD2K Processed Data/smoking-lvm-cleaned-data/'
+        save_filename = 'stress-episodes.csv'
+        if os.path.isfile(save_dir + save_filename):
+            append_write = 'a'  # append if already exists
+            header_binary = False
+        else:
+            append_write = 'w'  # make a new file if not
+            header_binary = True
+        temp_csv_file = open(save_dir+save_filename, append_write)
+        df.to_csv(temp_csv_file, header=header_binary, index=False)
+        temp_csv_file.close()
+        print('Added to stress label file!')
+        return None
