f9e58b42fc5114535f6ffd6b936c564ab324289c
wdempsey <dempsey.walter@gmail.com>
2019-10-29 11:59:46 -0400

    Fixing up the github to deal with the current mess of backups

--- data_preprocessing/unzip_backup_process.py
+++ data_preprocessing/unzip_backup_process.py
@@ -4,35 +4,27 @@ from unzip_backup_functions import *
 
 dir = '../data-streams-backup/Processed Phone Backup Files'
 
-# all_participant_ids = range(201, 223) + range(228,254)
-all_participant_ids = range(254, 271)
+all_participant_ids = range(201, 223) + range(228,271)
 
 participant_dates = pd.read_csv('../cleaned-data/participant-dates.csv')
 
 print('About to start the loop')
 
 for participant_id in all_participant_ids:
-        print('Now on participant ' + str(participant_id))
-        # File name we are dealing with
-        zip_name = '/'+str(participant_id)+'*.zip'
-        file_name = glob.glob(dir + zip_name)
-        file_exists = False
-        try:
-        	participant_zip = ZipFile(file_name[0])
-        	file_exists = True
-        except:
-        	print('participant '+ str(participant_id) + ' does not have cloud back up data!')
-        if not file_exists:
-        	continue
-        # ADD HQ PARTITION TO BACKUP FILE
-        study_days(participant_zip, participant_id, participant_dates)
-
-    	smoking_episode(participant_zip, participant_id)
-
-    	puff_probability(participant_zip, participant_id)
-
-    	end_of_day_ema(participant_zip, participant_id)
-
-    	random_ema(participant_zip, participant_id)
-
-    	event_contingent_ema(participant_zip, participant_id)
+    print('Now on participant ' + str(participant_id))
+    zip_name = '/'+str(participant_id)+'*.zip'
+    file_name = glob.glob(dir + zip_name)
+    file_exists = False
+    try:
+      	participant_zip = ZipFile(file_name[0])
+       	file_exists = True
+    except:
+       	print('participant '+ str(participant_id) + ' does not have cloud back up data!')
+    if not file_exists:
+       	continue
+    study_days(participant_zip, participant_id, participant_dates)
+    smoking_episode(participant_zip, participant_id)
+    puff_probability(participant_zip, participant_id)
+    end_of_day_ema(participant_zip, participant_id)
+    random_ema(participant_zip, participant_id)
+    event_contingent_ema(participant_zip, participant_id)
