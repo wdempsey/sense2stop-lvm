Fixed calculate_delta function; Fixed calculation of counts of self-reported smoking episodes

methods/setup-data.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import os
 
 # List down file paths
-dir_data = "../smoking-lvm-cleaned-data/final"
+dir_data = "../Processed Data/smoking-lvm-cleaned-data/final"
 
 # Read in data
 data_dates = pd.read_csv(os.path.join(os.path.realpath(dir_data), 'participant-dates.csv'))
@@ -69,10 +69,10 @@ data_selfreport["begin_unixts"] = data_selfreport["timestamp"]/1000
 def calculate_delta(message):
     sr_accptresponse = ['Smoking Event(less than 5 minutes ago)', 
                         'Smoking Event(5 - 15 minutes ago)', 
-                        'Smoking Event(15 to 30 minutes ago)']
+                        'Smoking Event(15 - 30 minutes ago)']
     sr_dictionary = {'Smoking Event(less than 5 minutes ago)': 2.5, 
-                     'Smoking Event(15 - 30 minutes ago)': 17.5, 
-                     'Smoking Event(5 - 15 minutes ago)': 10} 
+                     'Smoking Event(5 - 15 minutes ago)': 10,
+                     'Smoking Event(15 - 30 minutes ago)': 17.5} 
 
     if message in sr_accptresponse:
         # Convert time from minutes to seconds
@@ -145,16 +145,16 @@ data_selfreport["day_within_period"] = np.where(data_selfreport["is_post_quit"]<
 use_these_columns = ["participant_id", "start_date_unixts", "quit_date_unixts",
                      "expected_end_date_unixts","actual_end_date_unixts",
                      "is_post_quit", "study_day", "day_since_quit", "day_within_period",
-                     "begin_unixts", "smoked_unixts"]
+                     "begin_unixts", "message", "delta", "smoked_unixts"]
 data_selfreport = data_selfreport.loc[:, use_these_columns]
 
 #%%
 ###############################################################################
 # Write out csv file for prepared data if write_out==True
 ###############################################################################
-write_out = False
+write_out = True
 
 if write_out:
-    data_selfreport.to_csv(os.path.join(os.path.realpath(dir_data), 'work_with_data.csv'), index=False)
+    data_selfreport.to_csv(os.path.join(os.path.realpath(dir_data), 'work_with_datapoints.csv'), index=False)
 
 # %%

methods/simple-models-sr.py
@@ -7,11 +7,49 @@ from datetime import datetime
 import os
 
 # List down file paths
-dir_data = "../smoking-lvm-cleaned-data/final"
+dir_data = "../Processed Data/smoking-lvm-cleaned-data/final"
 
 # Read in data
 data_dates = pd.read_csv(os.path.join(os.path.realpath(dir_data), 'participant-dates.csv'))
 data_selfreport = pd.read_csv(os.path.join(os.path.realpath(dir_data), 'self-report-smoking-final.csv'))
+
+#%%
+# Define functions
+def calculate_delta(message):
+    sr_accptresponse = ['Smoking Event(less than 5 minutes ago)', 
+                        'Smoking Event(5 - 15 minutes ago)', 
+                        'Smoking Event(15 - 30 minutes ago)']
+    sr_dictionary = {'Smoking Event(less than 5 minutes ago)': 2.5, 
+                     'Smoking Event(15 - 30 minutes ago)': 17.5, 
+                     'Smoking Event(5 - 15 minutes ago)': 10} 
+
+    if message in sr_accptresponse:
+        # Convert time from minutes to seconds
+        use_delta = sr_dictionary[message]*60  
+    else:
+        # If participant reported smoking more than 30 minutes ago,
+        # then we consider time s/he smoked as missing
+        use_delta = pd.NA  
+    return use_delta
+
+def round_day(raw_day):
+    if pd.isna(raw_day):
+        # Missing values for raw_day can occur
+        # if participant reported smoking more than 30 minutes ago
+        out_day = pd.NA
+    else:
+        # This takes care of the instances when participant reported to smoke 
+        # less than 30 minutes ago
+        if raw_day >= 0:
+            # If on or after Quit Date, round down to the nearest integer
+            # e.g., floor(2.7)=2
+            out_day = np.floor(raw_day)
+        else:
+            # If before Quit Date, round up to the nearest integer
+            # e.g., ceil(-2.7)=-2
+            out_day = np.ceil(raw_day)
+        
+    return out_day
 
 #%%
 
@@ -51,61 +89,78 @@ data_dates = (
                  "start_date_unixts", "quit_date_unixts",
                  "expected_end_date_unixts","actual_end_date_unixts"]]
 )
+#%%
+###############################################################################
+# Data preparation: longdf_dates data frame
+###############################################################################
+longdf_dates = pd.DataFrame({'participant_id':[], 
+                             'start_date_unixts':[], 
+                             'quit_date_unixts':[],
+                             'expected_end_date_unixts':[],
+                             'actual_end_date_unixts':[],
+                             'quit_day':[], 
+                             'study_day':[]})
+n_participants = len(data_dates.index)
+
+for i in range(0, n_participants):
+    this_participant_dates = data_dates.iloc[i]
+    total_study_days = this_participant_dates["actual_end_date_unixts"] - this_participant_dates["start_date_unixts"]
+    total_study_days = total_study_days/(60*60*24)
+    total_study_days = np.int64(total_study_days)
+
+    if total_study_days > 0:
+        participant_id = np.int64(this_participant_dates["participant_id"])
+        start_date_unixts = np.int64(this_participant_dates["start_date_unixts"])
+        quit_date_unixts = np.int64(this_participant_dates["quit_date_unixts"])
+        expected_end_date_unixts = np.int64(this_participant_dates["expected_end_date_unixts"])
+        actual_end_date_unixts = np.int64(this_participant_dates["actual_end_date_unixts"])
+
+        quit_day = this_participant_dates["quit_date_unixts"] - this_participant_dates["start_date_unixts"]
+        quit_day = quit_day/(60*60*24)
+        quit_day = np.int64(quit_day)
+
+        this_participant_df = {'participant_id':np.repeat(participant_id, total_study_days),
+                               'start_date_unixts':np.repeat(start_date_unixts, total_study_days),
+                               'quit_date_unixts':np.repeat(quit_date_unixts, total_study_days),
+                               'expected_end_date_unixts':np.repeat(expected_end_date_unixts, total_study_days),
+                               'actual_end_date_unixts':np.repeat(actual_end_date_unixts, total_study_days),
+                               'quit_day':np.repeat(quit_day, total_study_days),
+                               'study_day':np.array(range(0, total_study_days))
+                              }
+        this_participant_df = pd.DataFrame(this_participant_df)
+        longdf_dates = longdf_dates.append(this_participant_df)
+    else:
+        pass
+
+#%%
+longdf_dates["day_since_quit"] = (
+    longdf_dates
+    .loc[:, ["study_day", "quit_day"]]
+    .pipe(lambda x: (x["study_day"] - x["quit_day"]))
+)
+
+longdf_dates["is_post_quit"] = (
+    longdf_dates
+    .loc[:, ["day_since_quit"]]
+    .pipe(lambda x: 1*(x["day_since_quit"]>=0))
+)
+
+longdf_dates["day_within_period"] = (
+    longdf_dates.loc[:, ["study_day","day_since_quit","is_post_quit"]]
+    .pipe(lambda x: x["is_post_quit"]*x["day_since_quit"] + (1-x["is_post_quit"])*x["study_day"])
+)
 
 #%%
 
 ###############################################################################
 # Merge data_selfreport with data_dates
 ###############################################################################
-data_selfreport = data_dates.merge(data_selfreport, 
-                                   how = 'left', 
-                                   on = 'participant_id')
-
-#%%
+data_selfreport = data_selfreport.merge(data_dates, how = 'left', on = 'participant_id')
 
 ###############################################################################
 # Data preparation: data_selfreport data frame
 ###############################################################################
 data_selfreport["begin_unixts"] = data_selfreport["timestamp"]/1000
-
-def calculate_delta(message):
-    sr_accptresponse = ['Smoking Event(less than 5 minutes ago)', 
-                        'Smoking Event(5 - 15 minutes ago)', 
-                        'Smoking Event(15 to 30 minutes ago)']
-    sr_dictionary = {'Smoking Event(less than 5 minutes ago)': 2.5, 
-                     'Smoking Event(15 - 30 minutes ago)': 17.5, 
-                     'Smoking Event(5 - 15 minutes ago)': 10} 
-
-    if message in sr_accptresponse:
-        # Convert time from minutes to seconds
-        use_delta = sr_dictionary[message]*60  
-    else:
-        # If participant reported smoking more than 30 minutes ago,
-        # then we consider time s/he smoked as missing
-        use_delta = pd.NA  
-    return use_delta
-
-def round_day(raw_day):
-    if pd.isna(raw_day):
-        # Missing values for raw_day can occur
-        # if participant reported smoking more than 30 minutes ago
-        out_day = pd.NA
-    else:
-        # This takes care of the instances when participant reported to smoke 
-        # less than 30 minutes ago
-        if raw_day >= 0:
-            # If on or after Quit Date, round down to the nearest integer
-            # e.g., floor(2.7)=2
-            out_day = np.floor(raw_day)
-        else:
-            # If before Quit Date, round up to the nearest integer
-            # e.g., ceil(-2.7)=-2
-            out_day = np.ceil(raw_day)
-        
-    return out_day
-
-#%%
-
 data_selfreport["delta"] = data_selfreport["message"].apply(lambda x: calculate_delta(x))
 data_selfreport["smoked_unixts"] = data_selfreport["begin_unixts"] - data_selfreport["delta"]
 
@@ -147,58 +202,90 @@ data_selfreport["day_within_period"] = np.where(data_selfreport["is_post_quit"]<
 use_these_columns = ["participant_id", "start_date_unixts", "quit_date_unixts",
                      "expected_end_date_unixts","actual_end_date_unixts",
                      "is_post_quit", "study_day", "day_since_quit", "day_within_period",
-                     "begin_unixts", "smoked_unixts"]
+                     "begin_unixts", "message", "delta", "smoked_unixts"]
 data_selfreport = data_selfreport.loc[:, use_these_columns]
 
 #%%
+###############################################################################
+# For each row in longdf_dates, count number of recorded selfreport timestamps
+###############################################################################
 
+# Loop through longdf_dates and count number of reported smoking events 
+# for each person day
+collect_count = []
+for i in range(0, len(longdf_dates.index)):
+    df_person_day = longdf_dates.iloc[i]
+    this_participant_id = df_person_day["participant_id"]
+    this_study_day = df_person_day["study_day"]
+    grabbed_data = data_selfreport[(data_selfreport.participant_id == this_participant_id) & (data_selfreport.study_day == this_study_day)]
+    this_count = len(grabbed_data.index)
+    collect_count.append(this_count)
+
+df_counts = longdf_dates
+df_counts = df_counts.assign(counts_sr = collect_count)
+
+#%%
+###############################################################################
+# In df_counts, identify which individuals have "trailing zeros"
+###############################################################################
+
+# Note: All rows belonging to individuals whose last 7 or erlier days consist
+# of "trailing zeros" are excluded
+
+aggregations = {"counts_sr": lambda x: 1 if sum(x.tail(7))>0 else 0}
+df_trailing_zeros = (
+    df_counts
+        .loc[:, ["participant_id","counts_sr"]]
+        .groupby("participant_id")
+        .agg(aggregations)
+        .reset_index(drop=False)
+        .rename(columns={"counts_sr": "keep_participant"})
+)
+
+df_counts_subset = df_counts.merge(df_trailing_zeros, how = 'left', on = 'participant_id')
+df_counts_subset = df_counts_subset[df_counts_subset.keep_participant==1].drop("keep_participant", axis=1)
+
+
+#%%
+# Exclude this participant
+df_counts_subset = df_counts_subset[df_counts_subset["participant_id"] != 227]
+
+#%%
+###############################################################################
+# Write out csv file for prepared data if write_out==True
+###############################################################################
+write_out = True
+
+if write_out:
+    df_counts.to_csv(os.path.join(os.path.realpath(dir_data), 'work_with_counts_all.csv'), index=False)
+    df_counts_subset.to_csv(os.path.join(os.path.realpath(dir_data), 'work_with_counts_subset.csv'), index=False)
+
+
+#%%
 ###############################################################################
 # Data preparation: Create data to be used as input to pymc3
 ###############################################################################
+
 # Collect data to be used in analyses in a dictionary
 collect_data_analysis = {}
-
-data_analysis = (
-    data_selfreport
-        .loc[:, ['participant_id','study_day','smoked_unixts']]
-        .groupby(['participant_id','study_day'])
-        .agg('count')
-        .reset_index(drop=False)
-        .rename(columns={"smoked_unixts": "count"})
-)
-
-collect_data_analysis['0'] = data_analysis
-
-data_analysis = (
-    data_selfreport
-        .loc[:, ['participant_id','is_post_quit','day_within_period','smoked_unixts']]
-        .groupby(['participant_id','is_post_quit','day_within_period'])
-        .agg('count')
-        .reset_index(drop=False)
-        .rename(columns={"smoked_unixts": "count"})
-)
-
-collect_data_analysis['1'] = data_analysis
-
-# Remove variable from workspace
-del data_analysis
-
-# collect_results is a dictionary that will collect results across all models
-collect_results={}
+collect_data_analysis['df_counts'] = df_counts
+collect_data_analysis['df_counts_subset'] = df_counts_subset
 
 #%%
+# collect_results is a dictionary that will collect results across all models
+collect_results={}
 
 ###############################################################################
 # Estimation using pymc3
 ###############################################################################
-use_this_data = collect_data_analysis['0']
+use_this_data = collect_data_analysis['df_counts_subset']
 
 with pm.Model() as model:
     # -------------------------------------------------------------------------
     # Data
     # -------------------------------------------------------------------------
     # Outcome Data
-    Y_observed = pm.Data('count', use_this_data['count'].values)
+    Y_observed = pm.Data('counts_sr', use_this_data['counts_sr'].values)
 
     # -------------------------------------------------------------------------
     # Priors
@@ -214,7 +301,7 @@ with pm.Model() as model:
 
 # Sample from posterior distribution
 with model:
-    posterior_samples = pm.sample(draws=3000, tune=7000, cores=1)
+    posterior_samples = pm.sample(draws=7000, tune=13000, cores=1)
 
 # Calculate 95% credible interval
 model_summary_logscale = az.summary(posterior_samples, credible_interval=.95)
@@ -245,14 +332,14 @@ del model, posterior_samples, model_summary_logscale, model_summary_expscale
 ###############################################################################
 # Estimation using pymc3
 ###############################################################################
-use_this_data = collect_data_analysis['1']
+use_this_data = collect_data_analysis['df_counts_subset']
 
 with pm.Model() as model:
     # -------------------------------------------------------------------------
     # Data
     # -------------------------------------------------------------------------
     # Outcome Data
-    Y_observed = pm.Data('count', use_this_data['count'].values)
+    Y_observed = pm.Data('counts_sr', use_this_data['counts_sr'].values)
     
     # Covariate Data
     is_post_quit = pm.Data('is_post_quit', use_this_data['is_post_quit'].values)
@@ -279,7 +366,7 @@ with pm.Model() as model:
 
 # Sample from posterior distribution
 with model:
-    posterior_samples = pm.sample(draws=3000, tune=7000, cores=1, max_treedepth=20)
+    posterior_samples = pm.sample(draws=7000, tune=13000, cores=1, max_treedepth=20)
 
 # Calculate 95% credible interval
 model_summary_logscale = az.summary(posterior_samples, credible_interval=.95)